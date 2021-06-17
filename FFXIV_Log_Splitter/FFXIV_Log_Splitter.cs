23d711055ffb142e189e82b811c3078f7c60d155
valarnin <valarnin@gmail.com>
2021-06-17 17:45:55 -0400

    Initial version

---
+++ FFXIV_Log_Splitter/FFXIV_Log_Splitter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+
+namespace FFXIV_Log_Splitter
+{
+    public class Log_Splitter_Plugin : IActPluginV1
+    {
+        private StreamWriter file;
+        private LogLineEventDelegate del;
+
+        public void DeInitPlugin()
+        {
+            ActGlobals.oFormActMain.BeforeLogLineRead -= del;
+            file.Close();
+        }
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            ((TabControl)pluginScreenSpace.Parent).TabPages.Remove(pluginScreenSpace);
+            var zone = ActGlobals.oFormActMain.CurrentZone;
+            var dateTime = DateTime.UtcNow;
+            file = new StreamWriter(getFilename(zone, dateTime), true);
+            file.AutoFlush = true;
+
+            del = (bool isImport, LogLineEventArgs logInfo) =>
+            {
+                try
+                {
+                    var line = logInfo.originalLogLine;
+                    var parts = line.Split('|');
+                    var et = uint.Parse(parts[0]);
+                    if (et == 1)
+                    {
+                        file.Close();
+                        zone = parts[3];
+                        dateTime = DateTime.Parse(parts[1]).ToUniversalTime();
+                        file = new StreamWriter(getFilename(zone, dateTime), true);
+                        file.AutoFlush = true;
+                    }
+                    file.WriteLine(line);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.ToString());
+                }
+            };
+
+            ActGlobals.oFormActMain.BeforeLogLineRead += del;
+
+            pluginStatusText.Text = "Initialized";
+        }
+
+        private string getFilename(string zone, DateTime dateTime)
+        {
+            var logFolder = Path.GetDirectoryName(ActGlobals.oFormActMain.LogFilePath);
+            var name = dateTime.ToString("yyyy-MM-ddTHH\\_mm\\_ss.fffffffzzz") + "_" + zone;
+            name = string.Join("_", name.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+            return logFolder + Path.DirectorySeparatorChar + "Zone_" + name + ".log";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ACT_FFXIV_Log_Splitter_Plugin
+# ACT FFXIV Log Splitter Plugin
+
+This plugin will automatically split FFXIV logs into `Zone_<DateTime>_<ZoneName>.log` files, in the same folder as normal FFXIV logs.
